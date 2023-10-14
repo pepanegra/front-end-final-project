6b1ec395b5102bf6ed08b77379da0e6bfdc3719f
Jonathan Angulo <jonathangabrielangulo95@gmail.com>
2023-10-14 12:32:26 -0500

    Cambios para produccion

--- main.js
+++ main.js
@@ -3,14 +3,19 @@ const $contenedorJugadores = d.querySelector(".jugadores");
 
 async function obtenerJugadores(api) {
   try {
-    let response = await fetch(api);
-    let data = await response.json();
+    const response = await fetch(api);
+    const data = await response.json();
 
     data.forEach((jugador) => {
       const $jugador = d.createElement("div");
-
+      $jugador.id = jugador.id;
+      $jugador.classList.add("jugador");
+      if (!jugador.imagen) {
+        jugador.imagen =
+          "https://thumbs.dreamstime.com/b/vector-de-perfil-avatar-predeterminado-foto-usuario-redes-sociales-desconocida-icono-desconocido-en-184816085.jpg";
+      }
       const jugadorPlantilla = `
-      <div class="jugador" id=${jugador.id}>
+    
       <img src="${jugador.imagen}" alt="${jugador.nombre}" class="imgj" />
       <div class="divinfo">
         <div>
@@ -56,7 +61,7 @@ async function obtenerJugadores(api) {
           </div>
         </div>
       </div>
-    </div>
+      <div><button class="btn-borrar"><img src="public/basura.png" alt="Borrar" class="img-borrar"></button></div> 
       `;
       $jugador.innerHTML = jugadorPlantilla;
       $contenedorJugadores.appendChild($jugador);
@@ -161,65 +166,16 @@ btnDc.addEventListener("click", () => {
 
 const btnC = d.querySelector(".button");
 
-btnC.addEventListener("click", (event) => {
+btnC.addEventListener("click", async (event) => {
   event.preventDefault();
 
-  // const headersList = {
-  //   Accept: "*/*",
-  //   "User-Agent": "Thunder Client (https://www.thunderclient.com)",
-  //   "Content-Type": "application/json",
-  // };
-
-  // const bodyContent = JSON.stringify({
-  //   nombre: d.querySelector("#nombre").value,
-  //   equipo: d.querySelector("#equipo").value,
-  //   posicion: d.querySelector("#posicion").value,
-  //   altura: d.querySelector("#altura").value,
-  //   peso: d.querySelector("#peso").value,
-  //   edad: d.querySelector("#edad").value,
-  //   nacionalidad: d.querySelector("#nacionalidad").value,
-  //   colegio: d.querySelector("#colegio").value,
-  //   draft: d.querySelector("#draft").value,
-  //   puntos_por_partido: d.querySelector("#puntos").value,
-  //   rebotes_por_partido: d.querySelector("#rebotes").value,
-  //   asistencias_por_partido: d.querySelector("#asistencias").value,
-  //   eficiencia: d.querySelector("#eficiencia").value,
-  //   imagen: d.querySelector("#imagen").value,
-  // });
-
-  // (async () => {
-  //   try {
-  //     const response = await fetch(
-  //       "https://jsonplaceholder.typicode.com/posts",
-  //       {
-  //         method: "POST",
-  //         body: bodyContent,
-  //         headers: headersList,
-  //       }
-  //     );
-
-  //     if (response.ok) {
-  //       const data = await response.json();
-  //       console.log("Respuesta exitosa:", data);
-  //     } else {
-  //       console.error(
-  //         "Error en la solicitud:",
-  //         response.status,
-  //         response.statusText
-  //       );
-  //     }
-  //   } catch (error) {
-  //     console.error("Error:", error);
-  //   }
-  // })();
-
-  let headersList = {
+  const headersList = {
     Accept: "*/*",
-    "User-Agent": "Thunder Client (https:www.thunderclient.com)",
+    "User-Agent": "hola",
     "Content-Type": "application/json",
   };
 
-  let bodyContent = JSON.stringify({
+  const bodyContent = JSON.stringify({
     nombre: d.querySelector("#nombre").value,
     equipo: d.querySelector("#equipo").value,
     posicion: d.querySelector("#posicion").value,
@@ -236,12 +192,62 @@ btnC.addEventListener("click", (event) => {
     imagen: d.querySelector("#imagen").value,
   });
 
-  let response = fetch("https://mackay-cassowary-kgtq.2.us-1.fl0.io/player", {
-    method: "POST",
-    body: bodyContent,
-    headers: headersList,
-  });
+  (async () => {
+    try {
+      const response = await fetch(
+        "https://mackay-cassowary-kgtq.2.us-1.fl0.io/player",
+        {
+          method: "POST",
+          body: bodyContent,
+          headers: headersList,
+        }
+      );
+
+      if (response.ok) {
+        const data = await response.json();
+        console.log("Respuesta exitosa:", data);
+      } else {
+        console.error(
+          "Error en la solicitud:",
+          response.status,
+          response.statusText
+        );
+      }
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  })();
 
   aside.classList.toggle("animation");
   console.log(d.querySelector("#nombre").value);
+  console.log(d.querySelector("#imagen").value);
+});
+
+const eliminar = d.querySelector(".btn-borrar");
+
+function deleteTask(id) {
+  fetch(`https://mackay-cassowary-kgtq.2.us-1.fl0.io/player${id}`, {
+    method: "DELETE",
+  })
+    .then((response) => {
+      console.log(response);
+      if (!response.ok) {
+        throw new Error("Ha ocurrido un error al eliminar la tarea");
+      }
+    })
+    .then(() => {
+      // Eliminar la tarea del DOM
+      document.getElementById(id).remove();
+      // Actualizar las estadísticas
+      updateStatics();
+      // Mostrar un mensaje de éxito
+      alert("Tarea eliminada correctamente");
+    })
+    .catch((error) => {
+      alert(error.message);
+    });
+}
+
+eliminar.addEventListener("click", async (e) => {
+  e.target.parent;
 });
